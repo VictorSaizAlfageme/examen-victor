4a55f8d0948a35ed422adf2189bb421a1b9a3212
Victor Saiz Alfageme <56159067+VictorSaizAlfageme@users.noreply.github.com>
2021-02-04 08:42:31 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -9,7 +9,7 @@ class Database {
 
 	function connect(){
 		//Copiar aquí también la info de la conexión
-		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$this->user="be9e2ab390ad38";$this->pass="2d53ab79";$this->host="eu-cdbr-west-03.cleardb.nett";$this->ddbb="heroku_3b310a6ef45c319";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
